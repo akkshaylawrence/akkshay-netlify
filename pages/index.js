d3b2ea84414026e321a7e7ded2563b350bdbefc3
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2021-01-13 20:12:35 +0530

    Updated readme and cleaned up project for development

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Akkshay Lawrence's Site</title>
+        <title>Akkshay Lawrence</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -16,7 +15,6 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-      <Footer />
     </div>
   )
 }
