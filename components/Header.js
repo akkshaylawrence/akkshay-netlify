c71476f2a45cd4bc8b5c91bf4171b5d88711bbec
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2021-01-15 00:19:30 +0530

    Fixed deploy

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-
 export default function Header() {
   return (
     <div className="flex flex-row justify-center items-center max-w-4xl mx-auto">
       <div className="flex-grow">
-        <Image src="/adithi.png" height={80} width={80}/>
+        <img src={"/adithi.png"} height={80} width={80} alt={"/adithi.png"}/>
       </div>
       <div className="text-gray-800"><h3>#screwperfect</h3></div>
     </div>
